de9875d2d08a9895ee59ada249d834198ba285fa
Itamar Hassin <ihassin@mac.com>
2017-05-14 09:15:47 -0400

    Added initial controller test that echos back the intent

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Amazon comes in with a post request
+  # resource :skills, only: [:create]
+  post '/' => 'skills#root', :as => :root
 end
