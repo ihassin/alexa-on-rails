1b85a8c1f6510ea330d9af0aeef759ed74ea399f
Itamar Hassin <ihassin@mac.com>
2017-05-26 09:22:02 -0400

    Parameterized city names

--- spec/controllers/skills_controller_spec.rb
+++ spec/controllers/skills_controller_spec.rb
@@ -5,14 +5,17 @@ RSpec.describe SkillsController, type: :controller do
 
   describe 'audio tests', :audio do
     it 'responds to ListOffice intent' do
-      Office.create [{ name: 'London' }, { name: 'Tel Aviv' }]
+      london = 'Paris'
+      aviv = 'Tel Aviv'
+
+      Office.create [{ name: london }, { name: aviv }]
 
       pid = play_audio 'spec/fixtures/list-office.m4a'
 
       client, data = start_server
 
       post :root, params: JSON.parse(data), format: :json
-      result = (response.body =~ /(?=London)(?=.*Aviv)/) > 0
+      result = (response.body =~ /(?=#{london})(?=.*#{aviv})/) > 0
 
       reply client, 'The list offices intent test ' + (result ? 'passed' : 'failed')
       expect(result).to be true
